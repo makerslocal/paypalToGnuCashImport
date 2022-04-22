facc58aea201d5cc156f3bfcaf276c8aee63a204
Kinsey Moore <nyphbl8d+github@gmail.com>
2022-04-21 21:21:15 -0500

    Add encoding change

--- paypalscript.py
+++ paypalscript.py
@@ -6,7 +6,7 @@ mapping = {"Date": "Date", "Name": "Name", "Amount": "Net", "Description": "Type
 def strip_row(row):
     return {k: row[v] for k,v in mapping.items()}
 
-with open("Paypal Export.csv", 'r') as filein:
+with open("Paypal Export.csv", 'r', encoding="utf-8-sig") as filein:
     csv_filein = [strip_row(row) for row in csv.DictReader(filein) if row["Balance Impact"] == "Credit"]
     with open('output.csv', 'w', newline='') as fileout:
         writer = csv.DictWriter(fileout, fieldnames=mapping.keys())
